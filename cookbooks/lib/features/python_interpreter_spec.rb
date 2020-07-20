10a4cddb03ac4fba39d9e24d7f52f16d89d3b56f
pawelgrzeda <60094765+pawelgrzeda@users.noreply.github.com>
2020-07-20 21:24:57 +0100

    Update python_interpreter_spec.rb

--- cookbooks/lib/features/python_interpreter_spec.rb
+++ cookbooks/lib/features/python_interpreter_spec.rb
@@ -1,24 +1,20 @@
 # frozen_string_literal: true
 
-shared_examples 'Readability counts' do |py|
+shared_examples 'python inrerpreter' do |py|
   describe command("#{py} -m this") do
     its(:stderr) { should be_empty }
     its(:stdout) { should match(/Readability counts\./) }
   end
+
+  describe command('python --version') do
+    its(:stderr) { should match(/^Python \d+\.\d+\.\d+/) }
+  end
 end
 
 describe 'python interpreter' do
-  describe command('python --version') do
-    its(:stderr) { should match(/^Python \d+\.\d+\.\d+/) }
-  end
-
-  include_examples 'Readability counts', 'python'
+  include_examples 'python inrerpreter', 'python'
 end
 
 describe 'python3 interpreter' do
-  describe command('python --version') do
-    its(:stderr) { should match(/^Python \d+\.\d+\.\d+/) }
-  end
-
-  include_examples 'Readability counts', 'python3'
+  include_examples 'python inrerpreter', 'python3'
 end
